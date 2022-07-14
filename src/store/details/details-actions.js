00bcf8244917dd650a12d7247b83b23497dcf2b1
khablo1508 <khablo.anna@gmail.com>
2022-07-14 21:21:45 +0200

    first commit

---
+++ src/store/details/details-actions.js
@@ -0,0 +1,46 @@
+export const SET_LOADING = '@@details/SET_LOADING';
+export const SET_ERROR = '@@details/SET_ERROR';
+export const SET_COUNTRY = '@@details/SET_COUNTRY';
+export const CLEAR_DETAILS = '@@details/CLEAR_DETAILS';
+export const SET_NEIGHBORS = '@@details/SET_NEIGHBORS';
+
+export const setLoading = () => ({
+  type: SET_LOADING,
+});
+export const setError = (error) => ({
+  type: SET_ERROR,
+  payload: error,
+});
+export const setCountry = (country) => ({
+  type: SET_COUNTRY,
+  payload: country,
+});
+export const clearDetails = () => ({
+  type: CLEAR_DETAILS,
+});
+const setNeighbors = (countries) => ({
+  type: SET_NEIGHBORS,
+  payload: countries,
+});
+
+export const loadCountryByName =
+  (name) =>
+  (dispatch, _, { client, api }) => {
+    dispatch(setLoading());
+    client
+      .get(api.searchByCountry(name))
+      .then(({ data }) => dispatch(setCountry(data[0])))
+      .catch((err) => setError(err.message));
+  };
+
+export const loadNeighborsByBorders =
+  (borders) =>
+  (dispatch, _, { client, api }) => {
+    client
+      .get(api.filterByCode(borders))
+      .then(({ data }) =>
+        dispatch(setNeighbors(data.map((country) => country.name))).catch(
+          console.error
+        )
+      );
+  };
